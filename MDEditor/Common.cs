23a704be56bafb2c6280f7d268764d630ee50b63
Acharean <Acharean@f0f459d6-1964-11de-ac02-0b9caec859f3>
2009-03-26 02:54:58 +0000

    Implementing database profiles and the splash screen

---
+++ MDEditor/Common.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDEditor
+{
+    internal delegate void Action();
+    internal delegate bool BoolAction();
+}
---
+++ MDEditor/Database/DBProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDEditor.Database
+{
+    internal class DBProfile
+    {
+        private string m_handle;
+
+        public string Handle
+        {
+            get { return m_handle; }
+        }
+    }
+}
