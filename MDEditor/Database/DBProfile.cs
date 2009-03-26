0194c1a41c2a95e0bd8eb3261d9accf4768b3feb
Acharean <Acharean@f0f459d6-1964-11de-ac02-0b9caec859f3>
2009-03-26 23:12:30 +0000

    More work on database profiles, saving / loading serialization

--- MDEditor/Database/DBProfile.cs
+++ MDEditor/Database/DBProfile.cs
@@ -1,16 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace MDEditor.Database
 {
+    [XmlElement]
     internal class DBProfile
     {
         private string m_handle;
+        private string m_characterUsername;
+        private string m_characterPassword;
+        private string m_accountUsername;
+        private string m_accountPassword;
 
+        public DBProfile()
+        {
+            m_handle = "";
+            m_characterPassword = "";
+            m_characterUsername = "";
+            m_accountPassword = "";
+            m_accountUsername = "";
+        }
+
+        [XmlAttribute]
         public string Handle
         {
             get { return m_handle; }
+            internal set { m_handle = value; }
+        }
+
+        [XmlAttribute]
+        public string CharacterUsername
+        {
+            get { return m_accountUsername; }
+            internal set { m_characterUsername = value; }
+        }
+
+        [XmlAttribute]
+        public string CharacterPassword
+        {
+            get { return m_accountPassword; }
+            internal set { m_characterPassword = value; }
+        }
+
+        [XmlAttribute]
+        public string AccountUsername
+        {
+            get { return m_accountUsername; }
+            internal set { m_accountUsername = value; }
+        }
+
+        [XmlAttribute]
+        public string AccountPassword
+        {
+            get { return m_accountPassword; }
+            internal set { m_accountPassword = value; }
         }
     }
 }
